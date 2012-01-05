9768fb1e4c3591bce58fa31c8451958e2baf33c8
ngdacphu.khtn <ngdacphu.khtn@gmail.com>
2012-01-05 16:53:54 +0000

    import core-service and website

---
+++ web/source/RSSMS_Site/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RSSMS_Site
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            CoreService.CoreServiceClient client = new CoreService.CoreServiceClient();
+            Label1.Text = client.GetData(5);
+        }
+    }
+}
