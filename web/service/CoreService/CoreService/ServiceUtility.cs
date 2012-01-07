aa2466a3ba086f2d719d6d73577715987ec70528
ngdacphu.khtn <ngdacphu.khtn@gmail.com>
2012-01-07 09:33:44 +0000

    implement method of the ICoreService

--- web/service/CoreService/CoreService/ServiceUtility.cs
+++ web/service/CoreService/CoreService/ServiceUtility.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CoreService
 {
     public class ServiceUtility
     {
+        public static string CalculateMD5Hash(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
 
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
     }
 }
