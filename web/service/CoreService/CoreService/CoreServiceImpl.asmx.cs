dc57dabd54bf85f868227a25f48f3e79b500ce00
ngdacphu.khtn <ngdacphu.khtn@gmail.com>
2012-01-07 07:11:16 +0000

    Add CoreService

---
+++ web/service/CoreService/CoreService/CoreServiceImpl.asmx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace CoreService
+{
+    /// <summary>
+    /// Summary description for CoreServiceImpl
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class CoreServiceImpl : System.Web.Services.WebService, ICoreService
+    {
+        #region Phu
+        [WebMethod]
+        public bool CheckValidUsername(string username)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool Register(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool Logout()
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool AddTab(Tab tab)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool RemoveTab(Tab tab)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool ShareTab(Tab tab, string userName)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool RenameTab(Tab tab, string newName)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public Tab[] GetAllTabs()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Phu
+
+        #region Deo
+        [WebMethod]
+        public bool AddRSSItem(Tab tab, RSSItem rssItem)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public bool RemoveRSSItem(Tab tab, RSSItem rssItem)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public RSSItem[] GetAllRSSItems(Tab tab)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public string GetRSSResult(RSSItem rssItem, int count)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Deo
+
+        #region Advance
+        [WebMethod]
+        public string GetRSSResultHTML(RSSItem rssItem, int count)
+        {
+            throw new NotImplementedException();
+        }
+        [WebMethod]
+        public string[] GetAllRSSPluginLink()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Advance
+    }
+}
---
+++ web/service/CoreService/CoreService/ICoreService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoreService
+{
+    public interface ICoreService
+    {
+        #region Phu
+        bool CheckValidUsername(string username);
+
+        bool Register(string username, string password);
+
+        bool Login(string username, string password);
+
+        bool Logout();
+
+        bool AddTab(Tab tab);
+
+        bool RemoveTab(Tab tab);
+
+        bool ShareTab(Tab tab, string userName);
+
+        bool RenameTab(Tab tab, string newName);
+
+        Tab[] GetAllTabs();
+        #endregion Phu
+
+        #region Deo
+        bool AddRSSItem(Tab tab, RSSItem rssItem);
+
+        bool RemoveRSSItem(Tab tab, RSSItem rssItem);
+
+        RSSItem[] GetAllRSSItems(Tab tab);
+
+        string GetRSSResult(RSSItem rssItem, int count);
+        #endregion Deo
+
+        string GetRSSResultHTML(RSSItem rssItem, int count);
+
+        string[] GetAllRSSPluginLink();
+    }
+}
