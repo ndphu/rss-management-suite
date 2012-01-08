347da96f49140e3e599fb4689042527e046611fa
DeoSieuMau <DeoSieuMau@21a6adb5-3793-0e32-657f-78ae0cacfe86>
2012-01-08 09:58:20 +0000

    Them DTO

---
+++ web/service/CoreService/CoreService/DTO/RSSItemDTO.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoreService
+{
+    public class RSSItemDTO
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _description;
+
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        private string _rSSLink;
+
+        public string RSSLink
+        {
+            get { return _rSSLink; }
+            set { _rSSLink = value; }
+        }
+        private int _tabID;
+
+        public int TabID
+        {
+            get { return _tabID; }
+            set { _tabID = value; }
+        }
+    }
+}
