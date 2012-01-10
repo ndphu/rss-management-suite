013025e234286733b8df71725bd82412b4b537ab
ngdacphu.khtn@gmail.com <ngdacphu.khtn@gmail.com@21a6adb5-3793-0e32-657f-78ae0cacfe86>
2012-01-10 18:20:06 +0000

    Delete bin & object Fix major problem in RSSForFIT :D

--- web/plugin/RSSForFIT/RSSForFIT/RSSForFIT.cs
+++ web/plugin/RSSForFIT/RSSForFIT/RSSForFIT.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using HtmlAgilityPack;
+
 using System.Xml;
+using System.Net;
+using System.IO;
+using System.Xml.Linq;
 
 namespace RSSForFIT
 {
@@ -11,8 +14,8 @@ namespace RSSForFIT
     {
         private static string websiteLinkStr = @"http://www.fit.hcmus.edu.vn/vn/";
         private static string pluginNameStr = "RSS for F.I.T - ĐH.KHTN";
-        private static string desciptionStr = "Diễn đàn tin tức - khoa Công nghệ thông tin - Đại học KHOA HỌC TỰ NHIÊN";
-        private static string rssNameStr = "Tin tức - Khoa CNTT - ĐH KHTN";
+        private static string desciptionStr = "Những tin tức nóng hổi nhất từ Trang chủ của Website môn học Khoa Công Nghệ Thông Tin - Trường Đại Học Khoa Học Tự Nhiên - Đại học Quốc gia TP Hồ Chính Minh";
+        private static string rssNameStr = "Khoa Công Nghệ Thông Tin _ Trường Đại Học Khoa Học Tự Nhiên";
 
         public string GetPluginName()
         {
@@ -36,124 +39,83 @@ namespace RSSForFIT
 
         public string GetRSSResult(int count)
         {
-            XmlDocument docXML = new XmlDocument();
-            try
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(websiteLinkStr);
+            StreamReader reader = new StreamReader(request.GetResponse().GetResponseStream());
+            string line = null;
+            StringBuilder sb = new StringBuilder();
+            bool b1 = false;
+            int check = 0;
+
+            while ((line = reader.ReadLine()) != null)
             {
-                HtmlWeb hw = new HtmlWeb();
-                HtmlDocument docHTML = hw.Load(websiteLinkStr);
-                HtmlNodeCollection listOfTable = docHTML.DocumentNode.SelectNodes("//table[@id='dnn_ctr989_ModuleContent']/tr/td/div/table");
-
-                CreateFITRSS(docXML, listOfTable, count);
-            }
-            catch
-            {
-                return CreateXmlErrorMessage("Eror!", "Server not found");
-            }
-            return FormatHTML(docXML.OuterXml);
-        }
-
-        private string FormatHTML(string result)
-        {
-            result = result.Replace("&lt;", "<");
-            result = result.Replace("&gt;", ">");
-            result = result.Replace("&amp;", "&");
-            return result;
-        }
-
-        private void CreateFITRSS(XmlDocument doc, HtmlNodeCollection listOfNode, int count)
-        {
-            CreateBasicRssDoc(doc, rssNameStr, desciptionStr, websiteLinkStr);
-            foreach (HtmlNode node in listOfNode)
-            {
-                CreateFITNodeInfo(doc, node);
-            }
-
-            XmlNode channelNode = doc.SelectSingleNode("//channel");
-            XmlNodeList listOfItemnNode = doc.SelectNodes(".//item");
-            int nItemNode = listOfItemnNode.Count;
-
-            for (int i = count; i < nItemNode; i++)
-            {
-                if (i < 0)
-                    continue;
-                channelNode.RemoveChild(listOfItemnNode[i]);
-            }
-        }
-
-        private void CreateBasicRssDoc(XmlDocument doc, string title, string description, string link)
-        {
-            XmlDeclaration declaration = doc.CreateXmlDeclaration("1.0", "utf-8", null);
-            doc.InsertBefore(declaration, doc.DocumentElement);
-
-            XmlElement rssNode = doc.CreateElement("rss");
-            XmlAttribute rssAtt = doc.CreateAttribute("version");
-            rssAtt.Value = "2.0";
-            rssNode.Attributes.Append(rssAtt);
-
-            XmlElement channelNode = doc.CreateElement("channel");
-
-            XmlElement titleNode = doc.CreateElement("title");
-            titleNode.InnerText = title;
-            channelNode.AppendChild(titleNode);
-
-            XmlElement desNode = doc.CreateElement("description");
-            desNode.InnerText = description;
-            channelNode.AppendChild(desNode);
-
-            XmlElement linkNode = doc.CreateElement("link");
-            linkNode.InnerText = link;
-            channelNode.AppendChild(linkNode);
-
-            rssNode.AppendChild(channelNode);
-            doc.AppendChild(rssNode);
-        }
-
-        private void CreateFITNodeInfo(XmlDocument doc, HtmlNode node)
-        {
-            XmlNode newItem = doc.CreateElement("item");
-
-            XmlNode titleNode = doc.CreateElement("title");
-            HtmlNode titleNodeOfHTML = node.SelectSingleNode("./tr/td[@class='post_title']");
-
-            for (int i = 0; i < titleNodeOfHTML.ChildNodes.Count; i++)
-            {
-                switch (titleNodeOfHTML.ChildNodes[i].Name)
+                line = line.Replace("&nbsp;", "").Replace("nowrap", "").Replace("&reg", "").Replace("&copy", "").Replace("&", "&amp;");
+                if (line.Contains("-->"))
+                    line = line.Substring(line.IndexOf("-->") + 3);
+                if (line.Contains("<div id=\"dnn_ctr989_ModuleContent\">"))
                 {
-                    case "a":
+                    b1 = true;
+                }
+
+                if (b1 == true)
+                {
+
+                    if (line.Contains("<div"))
+                    {
+                        ++check;
+                    }
+                    else if (line.Contains("</div>"))
+                    {
+                        --check;
+                        if (check == 0)
                         {
-                            titleNodeOfHTML.ChildNodes[i].Attributes[0].Value = websiteLinkStr + titleNodeOfHTML.ChildNodes[i].Attributes[0].Value;
+                            sb.Append(line.Substring(0, "</div>".Length));
+                            sb.Append("\r\n");
                             break;
                         }
-                    case "img":
-                        {
-                            titleNodeOfHTML.ChildNodes[i].Attributes[1].Value = websiteLinkStr + titleNodeOfHTML.ChildNodes[i].Attributes[1].Value;
-                            break;
-                        }
+                    }
+                    sb.Append(line);
+                    sb.Append("\r\n");
                 }
             }
-            titleNode.InnerText = titleNodeOfHTML.InnerHtml;
-            newItem.AppendChild(titleNode);
+            try
+            {
+                XDocument result = new XDocument(
+                    new XDeclaration("1.0", "utf-8", "yes"),
+                    new XElement("rss",
+                        new XAttribute("version", "2.0"),
+                        new XElement("channel",
+                            new XElement("title", rssNameStr),
+                            new XElement("description", desciptionStr),
+                            new XElement("link", websiteLinkStr)
+                            )
+                        )
+                     );
 
-
-            XmlNode descriptionNode = doc.CreateElement("description");
-            descriptionNode.InnerText = "";
-            newItem.AppendChild(descriptionNode);
-
-            XmlNode linkNode = doc.CreateElement("link");
-            linkNode.InnerText =  node.SelectSingleNode("./tr/td/a").Attributes[0].Value.ToString();
-            newItem.AppendChild(linkNode);
-
-            XmlNode pubDateNode = doc.CreateElement("pubDate");
-            int day = int.Parse(node.SelectSingleNode(".//tr[1]/td[@class='day_month']").InnerText.Trim());
-            int month = int.Parse(node.SelectSingleNode(".//tr[2]/td[@class='day_month']").InnerText.Trim());
-            int year = int.Parse(node.SelectSingleNode(".//td[@class='post_year']").InnerText.Trim());
-            DateTime dtDateTime = new DateTime(year, month, day);
-            pubDateNode.InnerText = dtDateTime.ToString("dd/MM/yyyy");
-            newItem.AppendChild(pubDateNode);
-
-            doc.DocumentElement.FirstChild.AppendChild(newItem);
+                XDocument xdoc = XDocument.Parse(sb.ToString());
+                var infos = xdoc.Element("div").Element("div").Elements("table").Elements("tr").Elements("td").Where(td => td.Attribute("class") != null && td.Attribute("class").Value.CompareTo("post_title") == 0).Select(_element => _element.Element("a"));
+                XElement linkElement = result.Element("rss").Element("channel").Element("link");
+                DateTime dateTime = DateTime.Now;
+                foreach (XElement element in infos)
+                {
+                    XElement itemElement = new XElement("item",
+                        new XElement("title", element.Attribute("title").Value),
+                        new XElement("description" , element.Value),
+                        new XElement("link", websiteLinkStr + element.Attribute("href").Value),
+                        new XElement("pubDate", dateTime.ToLongDateString() + " " + dateTime.ToLongTimeString()));
+                    linkElement.AddAfterSelf(itemElement);
+                }
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                return CreateXmlErrorMessage("Error", "Website " + websiteLinkStr + " is tempory unavailable.");
+            }
         }
 
+       
+       
+
+        
         private string CreateXmlErrorMessage(string error, string message)
         {
             XmlDocument doc = new XmlDocument();
