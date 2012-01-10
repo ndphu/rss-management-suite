2e7f12c2f5d0700da2202f508a5476f46ecfa178
DeoSieuMau <DeoSieuMau@21a6adb5-3793-0e32-657f-78ae0cacfe86>
2012-01-10 16:50:57 +0000

    - Sửa lại hàm InsertRss theo yêu cầu chi kiem tra exist trong 1 Tab - Viết thêm các hàm Advance: GetAllPlugin, AddRSSItemWithPlugin - Sửa lại hàm GetRSSResult để có thể lấy kết quả của RSSItem và RSSPluginItem

--- web/service/CoreService/CoreService/ICoreService.cs
+++ web/service/CoreService/CoreService/ICoreService.cs
@@ -43,6 +43,7 @@ namespace CoreService
 
         string[] GetAllRSSPluginLink(); // can remove this 
 
+        
         PluginDTO[] GetAllPlugin();
 
         int AddRSSItemWithPlugin(int tabid, int pluginID);
